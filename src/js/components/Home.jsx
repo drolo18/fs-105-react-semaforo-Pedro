fe18853bb7d60e4caa33349a9aba9c2fceac1838
Pedro Vélez <pedrolo.pv@gmail.com>
2025-05-01 08:57:45 +0000

    onNextColor fixed

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+const TRAFFIC_LIGHT = ["red", "yellow", "green"]
+
+
 const Home = () => {
 
-	const [ActualColor, setActualcolor] = useState()
+	const [ActualColor, setActualcolor] = useState(0)
 	const [showPurple, setShowPurple] = useState(false);
+	const [trafficLight, setTrafficLight] = useState(TRAFFIC_LIGHT);
 
-	const changetraffic = () => {
-        if (ActualColor === "red") {
-            setActualcolor("yellow")
-        } else if (ActualColor === "yellow") {
-            setActualcolor("green")
-        } else if (ActualColor === "green") {
-            setActualcolor("red")
+	const onNextColor = () => {
+        const newColor = ActualColor + 1;
+        if (newColor === trafficLight.length) {
+            setActualcolor(0)
+            return
         }
+        setActualcolor(newColor);
     }
 	const togglePurple = () => {
         setShowPurple(!showPurple)
@@ -29,14 +29,14 @@ const Home = () => {
 
 				</div>
 				<div className="traffic-light">
-					<div className={`red light ${ActualColor == "red" ? "glow" : ""}`} onClick={() => setActualcolor("red")} ></div>
-					<div className={`yellow light ${ActualColor == "yellow" ? "glow" : ""}`} onClick={() => setActualcolor("yellow")} ></div>
-					<div className={`green light ${ActualColor == "green" ? "glow" : ""}`} onClick={() => setActualcolor("green")} ></div>
+					<div className={`red light ${ActualColor == 0 ? "glow" : ""}`} onClick={() => setActualcolor(0)} ></div>
+					<div className={`yellow light ${ActualColor == 1 ? "glow" : ""}`} onClick={() => setActualcolor(1)} ></div>
+					<div className={`green light ${ActualColor == 2 ? "glow" : ""}`} onClick={() => setActualcolor(2)} ></div>
 					{showPurple && (<div className={`purple light ${ActualColor === "purple" ? "glow" : ""}`}onClick={() => setActualcolor("purple")}></div>)}
 				</div>
 			</div>
 			<div className="d-flex justify-content-center align-items-center ">
-			<button className="btn btn-primary btn-lg" onClick={changetraffic}>Change Traffic</button>
+			<button className="btn btn-primary btn-lg" onClick={onNextColor}>Change Traffic</button>
 			<button className="btn btn-secondary btn-lg" onClick={togglePurple}>{showPurple ? "Remove Purple" : "Add Purple"}</button>
 			</div>
 		</>
